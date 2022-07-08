70154ade1e6dec9e18dcc16dc03dda849f06fb5a
vp4t <104914018+vp4t@users.noreply.github.com>
2022-07-08 16:27:05 +0300

    2.17

---
+++ changePhone.js
@@ -0,0 +1,15 @@
+function replacePhone() {
+	var selectContactPhone = document.getElementsByName("ft-header-dropdown ft-header-contact-phones").textContent;
+		var active_Elements = document.activeElement;
+		var current_selection = active_Elements.value.substring(active_Elements.selectionStart, active_Elements.selectionEnd);
+		var currentCountry = document.getElementsByName("country").textContent;
+		var currentPhone = document.getElementsByName("phone").textContent;
+		var fieldPhone = document.getElementsByName("jquery-once-4-processed").textContent;
+					
+		selectContactPhone.addEventListener("click", function() {
+			if (currentCountry == "Australia" && currentPhone == "+61 2 6188 8118"){
+				document.getElementsByName("jquery-once-4-processed").textContent = '+06-1111-1111';
+				
+			}
+		});
+			}
