0acdc8c9afaec78336afa90d3b7007546a3a7f41
vp4t <104914018+vp4t@users.noreply.github.com>
2022-07-09 22:10:37 +0300

    2.18

--- changePhone.js
+++ changePhone.js
@@ -1,15 +1,15 @@
 debugger
 function replacePhone() {
-	var selectContactPhone = document.getElementsByName("ft-header-dropdown ft-header-contact-phones");
-		var active_Elements = document.activeElement;
-		var current_selection = active_Elements.value.substring(active_Elements.selectionStart, active_Elements.selectionEnd);
-		var currentCountry = active_Elements.getElementsByName("country");
-		var currentPhone = document.getElementsByName("phone").textContent;
-		var fieldPhone = document.getElementsByName("jquery-once-4-processed").textContent;
+		const selectContactPhone = document.querySelector(".ft-header-dropdown ft-header-contact-phones");
+		const active_Elements = document.activeElement;
+		const current_selection = active_Elements.value.substring(active_Elements.selectionStart, active_Elements.selectionEnd);
+		const currentCountry = active_Elements.querySelector(".country");
+		const currentPhone = document.querySelector(".phone");
+		const fieldPhone = document.querySelector(".jquery-once-4-processed");
 					
 		selectContactPhone.addEventListener("click", function() {
 			if (currentCountry == "Australia" && currentPhone == "+61 2 6188 8118"){
-				document.getElementsByName("jquery-once-4-processed").textContent = '+06-1111-1111';
+				document.querySelector(".jquery-once-4-processed") = '+06-1111-1111';
 				
 			}
 		});
