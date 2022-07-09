e7efd92b8811f1816bfc5b421135490f14d09c8d
vp4t <104914018+vp4t@users.noreply.github.com>
2022-07-09 23:18:18 +0300

    2.20

--- changePhone.js
+++ changePhone.js
@@ -1,12 +1,12 @@
-debugger
-function replace-Phone() {
+
+function replacePhone() {
 		const selectContactPhone = document.querySelector(".ft-header-dropdown ft-header-contact-phones");
 		const active_Elements = document.activeElement;
 		const current_selection = active_Elements.value.substring(active_Elements.selectionStart, active_Elements.selectionEnd);
 		const currentCountry = active_Elements.querySelector(".country");
 		const currentPhone = document.querySelector(".phone");
 		const fieldPhone = document.querySelector(".jquery-once-4-processed");
-					
+debugger				
 		selectContactPhone.addEventListener("click", function() {
 			if (currentCountry == "Australia" && currentPhone == "+61 2 6188 8118"){
 				document.querySelector(".jquery-once-4-processed") = '+06-1111-1111';
@@ -14,4 +14,4 @@ function replace-Phone() {
 			}
 		});
 			}
-replace-Phone()
+replacePhone()
